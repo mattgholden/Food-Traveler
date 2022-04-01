7302756c0bf11449f6e45c305038ca6f66af392d
mgholden <mgholden27@gmail.com>
2022-04-01 00:02:25 -0400

    Adding Navbar functions

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,8 +2,19 @@ import React from 'react'
 import './NavBar.css'
 import FoodTravelLogo from '../../FoodTravelLogo.png'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router';
 
 const NavBar = () => {
+
+  let navigate = useNavigate()
+
+  let handleClick = async() => {
+    navigate (`/register`)
+  }
+
+  let handleMouseClick = async() => {
+    navigate ('/login')
+  }
 
     return (
       <nav className="navbar is-info">
@@ -20,51 +31,14 @@ const NavBar = () => {
         </div>
         </div>
         <div>
-          <button className={`button is-primary is-rounded${'nav-button'}`}>Log In</button>
+          <button className={`button is-primary is-rounded${'nav-button'}`} onClick={handleMouseClick}>Log In</button>
           {/* <button className={`button is-danger is-rounded${'nav-button'}`} LogOut >Log Out</button> */}
-          <button className={`button is-rounded${'nav-button'}`}>Register</button>
+          <button className={`button is-rounded${'nav-button'}`} onClick={handleClick}>Register</button>
         </div>
     </nav>
   )
 }
-//   return(
-//  
 
-//   <div id="navbarExampleTransparentExample" className="navbar-menu">
-//     <div className="navbar-start">
-//       <Link className="navbar-item" to="/mapview">
-//         Food Traveler
-//       </Link>
-//     </div>
-
-//     <div className="navbar-end">
-//       <div className="navbar-item">
-//         <div className="field is-grouped">
-//           <p className="control">
-//             <Link className="bd-tw-button button" >
-//               <span className="icon">
-//                 <i className="fab fa-twitter"></i>
-//               </span>
-//               <span>
-//                 Login
-//               </span>
-//             </Link>
-//           </p>
-//           <p className="control">
-//             <Link className="button is-primary" to="/register">
-//               <span className="icon">
-//                 <i className="fas fa-download"></i>
-//               </span>
-//               <span>Register</span>
-//             </Link>
-//           </p>
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-// </nav>
-//   )
-// }
 
 export default NavBar
 
