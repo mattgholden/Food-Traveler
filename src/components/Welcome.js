f0b8549bdabbc87d160d735e8f3da58d39020fca
mgholden <mgholden27@gmail.com>
2022-04-02 10:33:23 -0400

    Changes to welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -11,7 +11,7 @@ return (
     <div>
       <div className='welcome-body'></div>  
       <div>
-        <h1 className='header'>Welcome, Food Travelers!  Here we aim to help make planning great meals while traveling easier.  Go ahead and get started planning your ToEat Lists by logging in or creating an account!</h1>
+        <h1 className='header'>Welcome, Food Travelers!  With this app, we help make planning great meals while you travel even easier.  Go ahead and get started planning your ToEat Lists by logging in or creating an account!</h1>
       </div>
     </div>
   );
